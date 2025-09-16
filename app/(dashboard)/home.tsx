0069d6cb101a9457321bf58402110a079b685317
Malik Bandara <malik.bandara@jaznu.com>
2025-09-16 11:11:26 +0530

    fix some alignment

--- app/(dashboard)/home.tsx
+++ app/(dashboard)/home.tsx
@@ -2,6 +2,7 @@ import { listRecipes } from "@/services/recipeService";
 import { Recipe } from "@/types/recipe";
 import { Link, useLocalSearchParams } from "expo-router";
 import React, { useEffect, useState } from "react";
+import { useIsFocused } from "@react-navigation/native";
 import {
   ActivityIndicator,
   Dimensions,
@@ -10,6 +11,7 @@ import {
   TouchableOpacity,
   View,
 } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const { height, width } = Dimensions.get("window");
 
@@ -18,6 +20,23 @@ export default function HomePage() {
   const [loading, setLoading] = useState(true);
 
   const params = useLocalSearchParams();
+
+  const isFocused = useIsFocused();
+
+  useEffect(() => {
+    if (isFocused) {
+      (async () => {
+        setLoading(true);
+        try {
+          const data = await listRecipes();
+          setRecipes(data);
+        } finally {
+          setLoading(false);
+        }
+      })();
+    }
+  }, [isFocused]);
+
   useEffect(() => {
     (async () => {
       setLoading(true);
@@ -39,7 +58,7 @@ export default function HomePage() {
   }
 
   return (
-    <View className="flex-1 bg-slate-900">
+    <SafeAreaView className="flex-1 bg-slate-900">
       {/* ---------- HEADER ---------- */}
       <View className="px-6 py-5 border-b border-slate-700 bg-slate-800">
         <Text className="text-3xl font-extrabold text-white">🍳 Cook Book</Text>
@@ -74,10 +93,7 @@ export default function HomePage() {
 
               {/* Description */}
               {item.description ? (
-                <Text
-                  className="mb-3 text-sm text-slate-400"
-                  numberOfLines={2}
-                >
+                <Text className="mb-3 text-sm text-slate-400" numberOfLines={2}>
                   {item.description}
                 </Text>
               ) : null}
@@ -93,8 +109,6 @@ export default function HomePage() {
           </Link>
         )}
       />
-
-      
-    </View>
+    </SafeAreaView>
   );
 }
